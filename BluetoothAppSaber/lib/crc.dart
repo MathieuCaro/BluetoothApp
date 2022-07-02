69542562054033d7faab64536e160ce2f63ee762
MathieuCaro <mathieucaro123@outlook.com>
2022-07-02 16:36:02 +0200

    Changement fichier1

--- BluetoothAppSaber/lib/crc.dart
+++ BluetoothAppSaber/lib/crc.dart
@@ -1,7 +1,7 @@
 import 'package:crclib/catalog.dart';
 
 List crc(List<int> crc) {
-  List<int> crcfinal = [];
+  List<int> crcfinal = []; //yo
   final crcstring = Crc16Arc().convert(crc).toString();
   int crcint = int.parse(crcstring);
   int hexacrc1 = crcint % 256;
